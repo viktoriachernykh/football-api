56a9ab1d48040d60f9c8138343088d966cd8412a
viktoriachernykh <chernycv@gmail.com>
2020-02-05 14:11:49 +0100

    Loggin message added

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.listen(port);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
